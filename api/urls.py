5f2b6b3aee3cafb5a2b1ad68c677805e3f929e97
NomadFool <nomad.fool@gmail.com>
2019-03-26 20:24:53 +0300

    detail api

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserCreateAPIView ,ItemListAPIView
+from .views import UserCreateAPIView ,ItemListAPIView, ItemDetailAPIView
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('login/', obtain_jwt_token, name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('list/', ItemListAPIView.as_view(), name = 'list')
+    path('list/', ItemListAPIView.as_view(), name = 'list'),
+    path('detail/<int:item_id>/', ItemDetailAPIView.as_view(), name = 'detail')
 ]
