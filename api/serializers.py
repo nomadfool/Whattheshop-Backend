efa47e05b527a874d7d1b66f020bf80b68b92df3
lilyBrainy <m.o.kh.alenzii@gmail.com>
2019-04-03 17:16:09 +0300

    user-profile

--- api/serializers.py
+++ api/serializers.py
@@ -1,21 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Item ,Category ,ThroughCartItemModel , Cart
+from .models import Item ,Category ,ThroughCartItemModel , Cart ,Address, Profile
+
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ['username','email','password']
+        fields = ['username','email','password' , 'first_name' , 'last_name']
 
     def create(self, validated_data):
         username = validated_data['username']
         email = validated_data['email']
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
         password = validated_data['password']
-        new_user = User(username=username, email=email)
+        new_user = User(username=username, email=email , first_name = first_name, last_name = last_name)
         new_user.set_password(password)
         new_user.save()
+        Profile.objects.create(user = new_user)
         return validated_data
 
 
@@ -55,3 +59,31 @@ class CheckOutSerializer(serializers.ModelSerializer):
 #     class Meta:
 #         model = Item
 #         fields =  '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+class AddressCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+    profile =  ProfileSerializer()
+    address = AddressSerializer()
+    class Meta:
+        model = User
+        fields = ['id','username' , 'first_name' , 'last_name' , 'email' , 'profile', 'address'] 
+
+
+                 
