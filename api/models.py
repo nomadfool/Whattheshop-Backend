931637652c04af894a3ed0097ee4770120cf6df3
NomadFool <nomad.fool@gmail.com>
2019-04-05 15:11:44 +0300

    deleted proplem migration files

--- api/models.py
+++ api/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from phone_field import PhoneField
+# from phone_field import PhoneField
 
 class Category(models.Model):
     name = models.CharField(max_length = 240)
